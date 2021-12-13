a680341c9170c8f00dde3dad60d67b8b4da52e54
hackwarrio <75233656+hackwarrio@users.noreply.github.com>
2021-12-13 15:20:54 -0500

    notificaciones

--- app/Http/Controllers/webController.php
+++ app/Http/Controllers/webController.php
@@ -9,21 +9,22 @@ use App\Events\RealTimeMessage;
 use Illuminate\Support\Facades\DB;
 use App\Models\Aw_user;
 use App\Models\Tarea;
+use Carbon\Carbon;
 
 class webController extends Controller
 {
     public function index()
     {
-        $giras = Gira::all();  
-        $i = 1;         
-        return view('giras.index',compact('giras','i'));
+        $giras = Gira::all();
+        $i = 1;
+        return view('giras.index', compact('giras', 'i'));
         // return view('welcome');
     }
     public function show($id_gira)
     {
         $gira = Gira::find($id_gira);
-        $etapas = Etapa::where('id_gira',$id_gira)->get();
-        return view('giras.show',compact('gira','etapas'));
+        $etapas = Etapa::where('id_gira', $id_gira)->get();
+        return view('giras.show', compact('gira', 'etapas'));
     }
     public function create()
     {
@@ -37,20 +38,20 @@ class webController extends Controller
             'estado' => 'required',
         ]);
         $gira = Gira::create([
-             'nombre'=>trim(strtoupper($request->nombre)),
-             'descripcion'=>$request->descripcion,
-             'estado'=>$request->estado,
+            'nombre' => trim(strtoupper($request->nombre)),
+            'descripcion' => $request->descripcion,
+            'estado' => $request->estado,
         ]);
         foreach ($request->etapas as $val) {
             Etapa::create([
-                'nombre'=>$val[0],
-                'color'=>$val[1],
-                'id_gira'=>$gira->id,
-            ]);            
+                'nombre' => $val[0],
+                'color' => $val[1],
+                'id_gira' => $gira->id,
+            ]);
         }
         return 'success';
     }
-    
+
     public function autocompletar(Request $request)
     {
 
@@ -61,7 +62,7 @@ class webController extends Controller
         $clientes = DB::table('aw_clientes')
             ->where('ruc', 'LIKE', '%' . $term . '%')
             ->orwhere('nombre', 'LIKE', '%' . $term . '%')
-            ->limit(10)   
+            ->limit(10)
             ->get();
 
         $response = array();
@@ -75,12 +76,55 @@ class webController extends Controller
         }
 
         return response()->json($response);
-    }    
-    public function crearTarea(Request $request){
+    }
+    public function crearTarea(Request $request)
+    {
         Tarea::create([
-            'tarea'=>$request->tarea,
-            'horario'=>$request->horario
+            'tarea' => $request->tarea,
+            'horario' => $request->horario
         ]);
         return 'success';
+    }
+    public function buscarNotificaciones()
+    {
+        $tareas = Tarea::where('estado',1)->get();
+        $notifications = [];
+
+        foreach ($tareas as  $tarea) {
+            array_push($notifications, [
+                'icon' => 'fas fa-fw fa-clock',
+                'text' => $tarea->tarea,
+                'time' => $tarea->created_at->diffForHumans(),
+            ]);
+        };
+
+        // Now, we create the notification dropdown main content.
+
+        $dropdownHtml = '';
+
+        foreach ($notifications as $key => $not) {
+            $icon = "<i class='mr-2 {$not['icon']}'></i>";
+
+            $time = "<span class='float-right text-muted text-sm'>
+                   {$not['time']}
+                 </span>";
+
+            $dropdownHtml .= "<a href='#' class='dropdown-item'>
+                            {$icon}{$not['text']}{$time}
+                          </a>";
+
+            if ($key < count($notifications) - 1) {
+                $dropdownHtml .= "<div class='dropdown-divider'></div>";
+            }
+        }
+
+        // Return the new notification data.
+
+        return [
+            'label'       => count($notifications),
+            'label_color' => 'danger',
+            'icon_color'  => 'warning',
+            'dropdown'    => $dropdownHtml,
+        ];
     }
 }
