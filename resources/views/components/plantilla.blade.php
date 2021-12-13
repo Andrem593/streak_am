d5d0492430cb52e4802f2af6aee9aefdfab3292c
hackwarrio <75233656+hackwarrio@users.noreply.github.com>
2021-12-13 14:20:13 -0500

    creacion de recordatorios

--- resources/views/components/plantilla.blade.php
+++ resources/views/components/plantilla.blade.php
@@ -15,11 +15,6 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/1.0.8/push.min.js" integrity="sha512-eiqtDDb4GUVCSqOSOTz/s/eiU4B31GrdSb17aPAA4Lv/Cjc8o+hnDvuNkgXhSI5yHuDvYkuojMaQmrB5JB31XQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
-<<<<<<< HEAD
-        window.Echo.channel('events').listen('RealTimeMessage', (e) => {
-            console.log('RealTimeMessage: ' + e.message)
-        });        
-=======
         window.Echo.channel('events').listen('RealTimeMessage', (e) => {           
             Push.create("Tienes un Recordatorio:", {
                 body: e.message,
@@ -29,7 +24,6 @@
                 }
             });
         });
->>>>>>> edaa85de4009c4f6dd7c7330594df0ceff002541
     </script>
     @livewireScripts()
 @stop
