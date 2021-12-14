4c92212900e4d3f75216202ec8b4b1c30b857cf3
hackwarrio <75233656+hackwarrio@users.noreply.github.com>
2021-12-14 15:08:22 -0500

    offcanvas

--- app/Http/Livewire/OffCanvas.php
+++ app/Http/Livewire/OffCanvas.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class OffCanvas extends Component
 {
-    public $open = false;
+    public $open = false, $clientes,$buscar;
 
     public function render()
     {
+        $this->clientes = DB::table('aw_clientes')->Where('nombre','LIKE','%'.$this->buscar.'%')->get();
         return view('livewire.off-canvas');
     }
 }
--- resources/views/livewire/off-canvas.blade.php
+++ resources/views/livewire/off-canvas.blade.php
@@ -1,17 +1,46 @@
 <div>
-    <button class="btn btn-primary btn-sm offcanvas-trigger" wire:click="$set('open',true)"><i class="fas fa-plus mr-1"></i>Agregar
-        Cliente</button>  
-
-    <div id="canvas" class="offcavas shadow {{ $open == true ? 'move-to-init' : 'move-to-left'}}">
-        <div class="offcanvas-header">
-            <h5 id="offcanvasRightLabel">Offcanvas right</h5>
-            
+    {{-- elemento a mostrar boton abrir off canvas --}}
+    <button class="btn btn-primary btn-sm offcanvas-trigger" wire:click="$set('open',true)"><i
+            class="fas fa-plus mr-1"></i>Agregar
+        Cliente</button>
+    {{-- elemento off canvas --}}
+    <div id="canvas" class="offcavas shadow {{ $open == true ? 'move-to-init' : 'move-to-left' }}">
+        <div class="offcanvas-header p-2">
+            <h5>AGREGAR CLIENTES</h5>
         </div>
-        <div class="offcanvas-body">
-            
+        <div class="offcanvas-body p-2">
+            <div class="row">
+                <div class="col">
+                    <div class="input-group input-group-sm">
+                        <input id="txt_search" wire:model="buscar" class="form-control form-control-divbar" type="text"
+                            placeholder="Buscar cliente" aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-info" type="button">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row py-1">
+                <div class="col">
+                    <div class="list-group" id="myList" role="tablist">
+                        
+                        @empty(!$clientes)
+                            @foreach ($clientes as $cliente)
+                                <a class="list-group-item list-group-item-action p-2" href="#home" role="tab"><button
+                                        class="btn btn-sm btn-primary"><i class="fas fa-plus"></i></button><span
+                                        class="pl-2">{{ $cliente->nombre }}</span></a>
+                            @endforeach
+                        @endempty
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div id="overley" class="{{ $open == true ? '' : 'd-none'}}" wire:click="$set('open',false)" style="width: 100%;height: 100%;">
+    {{-- overley con funciones de click para desaparecer --}}
+    <div id="overley" class="{{ $open == true ? '' : 'd-none' }}" wire:click="$set('open',false)"
+        style="width: 100%;height: 100%;">
 
     </div>
 </div>
