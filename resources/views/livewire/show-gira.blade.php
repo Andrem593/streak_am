1a5c90c5591345d6cfdd96327bb0e13c29b6ef2e
Christian Ramos <crismas06@outlook.com>
2021-12-16 17:26:20 -0500

    seleccion masiva etapas

--- resources/views/livewire/show-gira.blade.php
+++ resources/views/livewire/show-gira.blade.php
@@ -90,7 +90,7 @@
                                                         @foreach ($clientes_x_etapa as $cliente)
                                                             @if ($etapa->id == $cliente->id_etapa)
                                                                 <tr>
-                                                                    <td><a href="{{                                                                     <td><a href="route('fase.historial', ['id_cliente' => $cliente->id_cliente, 'id_etapa' => $cliente->id_etapa, 'id_gira' => $id_gira]) }}"
+                                                                    <td><a href="{{route('fase.historial', ['id_cliente' => $cliente->id_cliente, 'id_etapa' => $cliente->id_etapa, 'id_gira' => $id_gira]) }}"
                                                                             class="btn btn-sm btn-info"><i
                                                                                 class="fas fa-edit"></i></a>
                                                                     </td>
