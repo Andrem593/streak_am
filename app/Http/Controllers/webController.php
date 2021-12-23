0758511ca5c3d9dc3c2dda8eb1d3b2b9aab7f124
Christian Ramos <crismas06@outlook.com>
2021-12-23 12:46:23 -0500

    buscador de clientes

--- app/Http/Controllers/webController.php
+++ app/Http/Controllers/webController.php
@@ -27,17 +27,17 @@ class webController extends Controller
                 ->where('id_usuario', session('id_usuario'))
                 ->first();
         }
-        
-        $giras = Gira::leftJoin('aw_users','aw_users.id_usuario','=','giras.id_usuario')->select('giras.*','aw_users.nombre_usuario');
-        if($user->tipo_usuario == 'vendedor'){
+
+        $giras = Gira::leftJoin('aw_users', 'aw_users.id_usuario', '=', 'giras.id_usuario')->select('giras.*', 'aw_users.nombre_usuario');
+        if ($user->tipo_usuario == 'vendedor') {
             $giras->where('aw_users.id_usuario', session('id_usuario'));
         }
         if (!empty($_GET['id_cliente'])) {
-            $giras->join('etapas','etapas.id_gira','=','giras.id')->join('etapa_has_clientes','etapa_has_clientes.id_etapa','=','etapas.id')->join('aw_clientes','aw_clientes.id_cliente','=','etapa_has_clientes.id_cliente')->where('aw_clientes.id_cliente', $_GET['id_cliente']);
+            $giras->join('etapas', 'etapas.id_gira', '=', 'giras.id')->join('etapa_has_clientes', 'etapa_has_clientes.id_etapa', '=', 'etapas.id')->join('aw_clientes', 'aw_clientes.id_cliente', '=', 'etapa_has_clientes.id_cliente')->where('aw_clientes.id_cliente', $_GET['id_cliente']);
         }
         $giras = $giras->get();
-        $i = 1;        
-        return view('giras.index', compact('giras', 'i','user'));
+        $i = 1;
+        return view('giras.index', compact('giras', 'i', 'user'));
     }
     public function show($id_gira)
     {
@@ -50,8 +50,8 @@ class webController extends Controller
     public function create()
     {
         $gira = new Gira();
-        $usuarios = DB::table('aw_users')->where('tipo_usuario','vendedor')->get(['id_usuario','nombre_usuario']);
-        return view('giras.create', compact('gira','usuarios'));
+        $usuarios = DB::table('aw_users')->where('tipo_usuario', 'vendedor')->get(['id_usuario', 'nombre_usuario']);
+        return view('giras.create', compact('gira', 'usuarios'));
     }
     public function createGira(Request $request)
     {
@@ -63,9 +63,9 @@ class webController extends Controller
             'nombre' => trim(strtoupper($request->nombre)),
             'descripcion' => $request->descripcion,
             'estado' => $request->estado,
-            'id_usuario'=>$request->vendedor,
+            'id_usuario' => $request->vendedor,
         ]);
-        foreach ($request->etapas as $i =>$val) {
+        foreach ($request->etapas as $i => $val) {
             Etapa::create([
                 'nombre' => $val[0],
                 'color' => $val[1],
@@ -81,22 +81,22 @@ class webController extends Controller
             'nombre' => 'required|max:50|min:5',
             'estado' => 'required',
         ]);
-        Gira::where('id',$request->id)->update([
-            'nombre'=>$request->nombre,
-            'descripcion'=>$request->descripcion,
-            'estado'=>$request->estado,
+        Gira::where('id', $request->id)->update([
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'estado' => $request->estado,
         ]);
         foreach ($request->etapas as $i => $val) {
             if ($val[2] != '') {
-                Etapa::where('id',$val[2])->update([
-                    'orden'=>$i
+                Etapa::where('id', $val[2])->update([
+                    'orden' => $i
                 ]);
-            }else{
+            } else {
                 Etapa::create([
-                    'nombre'=>$val[0],
-                    'color'=>$val[1],
-                    'orden'=>$i,
-                    'id_gira'=>$request->id,
+                    'nombre' => $val[0],
+                    'color' => $val[1],
+                    'orden' => $i,
+                    'id_gira' => $request->id,
                 ]);
             }
         }
@@ -105,50 +105,55 @@ class webController extends Controller
     public function edit($id_gira)
     {
         $gira = Gira::find($id_gira);
-        $estados = ['Activa','Inactiva','Cancelada','Completada'];
-        $etapas = Etapa::where('id_gira',$id_gira)->orderBy('orden')->get();
-        return view('giras.edit', compact('gira','estados','etapas'));
+        $estados = ['Activa', 'Inactiva', 'Cancelada', 'Completada'];
+        $etapas = Etapa::where('id_gira', $id_gira)->orderBy('orden')->get();
+        return view('giras.edit', compact('gira', 'estados', 'etapas'));
     }
 
     public function autocompletar(Request $request)
     {
         $data = $request->all();
 
-        $term = $data['term'];        
-
-        $clientes = DB::table('aw_clientes')
-            ->where('aw_clientes.ruc', 'LIKE', '%' . $term . '%')
-            ->orwhere('aw_clientes.nombre', 'LIKE', '%' . $term . '%')
-            ->limit(10);
-
-        $user = DB::table('aw_users')
-            ->where('id_usuario', session('id_usuario'))
-            ->first();
-        
-        if(!empty($data['only'])){
-            $only = $data['only'];
-            if($only == 'etapaCliente'){
-                $clientes->join('etapa_has_clientes','etapa_has_clientes.id_cliente','=','aw_clientes.id_cliente')->join('etapas','etapas.id','=','etapa_has_clientes.id_etapa')->join('giras','giras.id','=','etapas.id_gira')->select('aw_clientes.*','etapas.id as id_etapa','giras.id as id_gira');
-                if($user->tipo_usuario == 'vendedor'){
-                    $clientes->where('giras.id_usuario', $user->id_usuario);
-                }
-            }
-        }
-        
-        $clientes = $clientes->get();
+        $term = $data['term'];
         $response = array();
 
-        if(!empty($data['only'])){
-            foreach ($clientes as $cliente) {
-                $response[] = array("value" => $cliente->nombre, "ruc" => $cliente->ruc, "id" => $cliente->id_cliente, "id_etapa" => $cliente->id_etapa, "id_gira" => $cliente->id_gira);
+        if (!empty($term)) {
+            $clientes = DB::table('aw_clientes')
+                ->where('aw_clientes.ruc', 'LIKE', '%' . $term . '%')
+                ->orwhere('aw_clientes.nombre', 'LIKE', '%' . $term . '%')
+                ->limit(10);
+
+            $user = DB::table('aw_users')
+                ->where('id_usuario', session('id_usuario'))
+                ->first();
+
+            if (!empty($data['only'])) {
+                $only = $data['only'];
+                if ($only == 'etapaCliente') {
+                    $clientes->join('etapa_has_clientes', 'etapa_has_clientes.id_cliente', '=', 'aw_clientes.id_cliente')->join('etapas', 'etapas.id', '=', 'etapa_has_clientes.id_etapa')->join('giras', 'giras.id', '=', 'etapas.id_gira')->select('aw_clientes.*', 'etapas.id as id_etapa', 'giras.id as id_gira');
+                    if ($user->tipo_usuario == 'vendedor') {
+                        $clientes->where('giras.id_usuario', $user->id_usuario);
+                    }
+                }
+            }
+
+            $clientes = $clientes->get();
+            
+
+            if (!empty($data['only'])) {
+                foreach ($clientes as $cliente) {
+                    $response[] = array("value" => $cliente->nombre, "ruc" => $cliente->ruc, "id" => $cliente->id_cliente, "id_etapa" => $cliente->id_etapa, "id_gira" => $cliente->id_gira);
+                }
+            } else {
+                foreach ($clientes as $cliente) {
+                    $response[] = array("value" => $cliente->nombre, "ruc" => $cliente->ruc, "id" => $cliente->id_cliente);
+                }
+            }
+
+            if (count($response) == 0) {
+                $response[] = array("value" => "");
             }
         }else{
-            foreach ($clientes as $cliente) {
-                $response[] = array("value" => $cliente->nombre, "ruc" => $cliente->ruc, "id" => $cliente->id_cliente);
-            }
-        }
-
-        if (count($response) == 0) {
             $response[] = array("value" => "");
         }
 
@@ -157,11 +162,11 @@ class webController extends Controller
     public function crearTarea(Request $request)
     {
         Tarea::create([
-            'id_usuario'=>session('id_usuario'),
+            'id_usuario' => session('id_usuario'),
             'tarea' => $request->tarea,
             'horario' => $request->horario,
-            'tipo_gestion'=>$request->tipo_gestion,
-            'nombre_cliente'=>$request->nombre_cliente,
+            'tipo_gestion' => $request->tipo_gestion,
+            'nombre_cliente' => $request->nombre_cliente,
         ]);
         return 'success';
     }
@@ -169,22 +174,22 @@ class webController extends Controller
     {
         $fecha_actual = Carbon::now();
         $fecha_actual = $fecha_actual->toDateTimeString();
-        Tarea::where('horario','<=',$fecha_actual)->where('estado',1)->update([
-            'estado'=> 4
+        Tarea::where('horario', '<=', $fecha_actual)->where('estado', 1)->update([
+            'estado' => 4
         ]);
         // verificar si debe enviar correo
-        $enviarEmail = Tarea::where('estado',4)->get();
-        foreach ($enviarEmail as $value) {     
-            $usuario = DB::table('aw_users')->where('id_usuario',$value->id_usuario)->first();            
+        $enviarEmail = Tarea::where('estado', 4)->get();
+        foreach ($enviarEmail as $value) {
+            $usuario = DB::table('aw_users')->where('id_usuario', $value->id_usuario)->first();
             $correo = new RecordatorioMailable($value);
-            Mail::to($usuario->usuario)->send($correo);   
-            Tarea::where('estado',4)->update([
-                'estado'=> 2
+            Mail::to($usuario->usuario)->send($correo);
+            Tarea::where('estado', 4)->update([
+                'estado' => 2
             ]);
         }
 
-        $tareas = Tarea::where('estado',2)->where('id_usuario',session('id_usuario'))
-        ->get();
+        $tareas = Tarea::where('estado', 2)->where('id_usuario', session('id_usuario'))
+            ->get();
         $notifications = [];
 
         foreach ($tareas as  $tarea) {
@@ -193,9 +198,9 @@ class webController extends Controller
                 'text' => $tarea->tarea,
                 'time' => $tarea->created_at->diffForHumans(),
                 'id'   => $tarea->id,
-                'nombre_cliente'=>$tarea->nombre_cliente,
+                'nombre_cliente' => $tarea->nombre_cliente,
             ]);
-        };        
+        };
         // Now, we create the notification dropdown main content.
 
         $dropdownHtml = '';
@@ -232,41 +237,42 @@ class webController extends Controller
             'dropdown'    => $dropdownHtml,
         ];
     }
-    public function notificaciones(){
-        $tareas = Tarea::where('estado',3)->OrWhere('estado',2)->where('id_usuario',session('id_usuario'))->get();
-        return view('recordatorios.index' ,compact('tareas'));
+    public function notificaciones()
+    {
+        $tareas = Tarea::where('estado', 3)->OrWhere('estado', 2)->where('id_usuario', session('id_usuario'))->get();
+        return view('recordatorios.index', compact('tareas'));
     }
     public function marcarLeida($id_notificacion)
     {
-        $tarea = Tarea::where('id',$id_notificacion)->update([
-            'estado'=>3,
+        $tarea = Tarea::where('id', $id_notificacion)->update([
+            'estado' => 3,
         ]);
 
         return 'success';
     }
     public function validacionClientesEtapa(Request $request)
     {
-        $etapa = EtapaHasCliente::where('id_etapa',$request->id)->get();
-        if ($etapa->count() == 0 ) {
-            Etapa::where('id',$request->id)->delete();
+        $etapa = EtapaHasCliente::where('id_etapa', $request->id)->get();
+        if ($etapa->count() == 0) {
+            Etapa::where('id', $request->id)->delete();
         }
         return $etapa->count();
-
     }
 
     public function reporte()
     {
         $response = '';
-        $comment = Comentario::where('id_usuario',session('id_usuario'))->join('aw_clientes','aw_clientes.id_cliente','=','comentarios.id_cliente')
-        ->select('comentarios.*','aw_clientes.*')->get();
+        $comment = Comentario::where('id_usuario', session('id_usuario'))->join('aw_clientes', 'aw_clientes.id_cliente', '=', 'comentarios.id_cliente')
+            ->select('comentarios.*', 'aw_clientes.*')->get();
         if (count($comment) == 0) {
             $comment = 'no data';
         }
         $response = json_encode($comment);
         return $response;
     }
-    public function eliminarTarea(Request $request){
-        Tarea::where('id',$request->id_tarea)->delete();
+    public function eliminarTarea(Request $request)
+    {
+        Tarea::where('id', $request->id_tarea)->delete();
         return 'success';
     }
 }
