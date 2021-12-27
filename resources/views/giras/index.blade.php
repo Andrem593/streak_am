e8c2941be0c1cbe0b297f8ea03610749161768e3
hackwarrio <75233656+hackwarrio@users.noreply.github.com>
2021-12-27 11:38:04 -0500

    modificacion de color y estilos

--- resources/views/giras/index.blade.php
+++ resources/views/giras/index.blade.php
@@ -27,14 +27,7 @@
                 <p>GIRA EDITADA CORRECTAMENTE</p>
             </div>
         @endif
-        @if (!empty(session($mensaje)))
-        {{dd(session($mensaje))}}
-            <div class="alert alert-danger">
-                <p>{{$mensaje}}</p>
-            </div>
-        @endif
-        <div class="card">
-            <div class="card-header">
+
         <div class="card shadow-lg redondeado">
             <div class="card-header bg-success redondeado-card">
                 <h3 class="card-title">Lista de Giras Actuales</h3>
