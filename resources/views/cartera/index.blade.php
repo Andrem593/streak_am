d4e59eee84890482b07fd0847f67266c75c6861b
Christian Ramos <crismas06@outlook.com>
2021-12-27 12:34:57 -0500

    cartera clientes

--- resources/views/cartera/index.blade.php
+++ resources/views/cartera/index.blade.php
@@ -6,10 +6,10 @@
                 <h2 class="fw-bold my-2" style="font-size: 20px">Cartera de Clientes</h2>
             </div>
             <div class="col my-auto text-right">
-                <a class="btn btn-warning btn-sm" href="{{ public_path('resources/cartera_clientes.xlsx') }}" target="_blank"><i class="fas fa-file-alt"></i>
+                <a class="btn btn-warning btn-sm" href="{{ public_path('resources/cartera_clientes.xlsx') }}" target="_blank"><i class="fas fa-file-excel"></i>
                     Plantilla</a>
-                <a class="btn btn-primary btn-sm" href=""><i class="fas fa-file-alt"></i>
-                    Regresar</a>
+                    <a class="btn btn-primary btn-sm" href="{{ redirect()->back()->getTargetUrl() }}"><i
+                        class="fas fa-arrow-left mr-1"></i>Regresar</a>
             </div>
         </div>
     </div>
